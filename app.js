af3da806f0a4f8a2a11461a8405c816c92e77d98
rahul4172 <rahulshelke4172@gmail.com.com>
2022-11-06 09:40:23 +0530

    Delete request added

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set('view engine','ejs');
 
 // Middleware and static files 
 app.use(express.static('public'));
-
+app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 
 
@@ -89,6 +89,46 @@ app.get('/blogs',(req,res)=>{
         console.log(err);
     });
 });
+
+app.post('/blogs',(req,res)=>{
+   
+    const blog = new Blog(req.body);
+    blog.save()
+    .then((result)=>{
+        res.redirect('/blogs');
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+});
+
+app.get('/blogs/:id',(req,res)=>{
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result=>{
+        res.render('details',{ blog:result , title:'Blog details'});
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+});
+
+// delete request
+
+app.delete('/blogs/:id',(req,res)=>{
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result=>{
+       res.json({redirect : '/blogs'});
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+});
+
+
 
 
 app.get('/about', (req,res) => {
